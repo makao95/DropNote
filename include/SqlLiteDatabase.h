7eae8cb31a23eecfb5bd4b5b5e3deb62d56eaf56
Marcin Dawidziuk <cinekdawidziuk@gmail.com>
2016-07-18 14:16:24 +0200

    Deleted unused QSLite wrapper

--- include/SqlLiteDatabase.h
+++ include/SqlLiteDatabase.h
@@ -8,45 +8,5 @@
 #include <QSqlQuery>
 
 namespace sqlite{
-std::string nowDateTime();
-
 QString getLastExecutedQuery(const QSqlQuery& query);
-
 }
-
-class SqlLiteException : std::exception {
-public:
-    SqlLiteException(std::string msg);
-    SqlLiteException(std::string msg, std::string query);
-    virtual const char* what() const;
-private:
-    std::string msg;
-};
-
-class SqlRow{
-public:
-    std::string operator[](std::string);
-    std::string operator[](int);
-
-    std::string getString(int index);
-    std::string getString(std::string name);
-    int getInt(int index);
-    int getInt(std::string name);
-    float getFloat(int index);
-    float getFloat(std::string name);
-
-    void push_back(std::string name, std::string value);
-private:
-    std::unordered_map<std::string, std::string> row;
-
-};
-
-class SqlLiteDatabase{
-public:
-    bool open(std::string filename);
-    bool query(std::string query);
-    std::vector<SqlRow> select(std::string query);
-private:
-    sqlite3* db;
-};
-
